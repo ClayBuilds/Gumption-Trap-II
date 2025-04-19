62718f3ce6d8e8a29c363d7a9b6beb0b94cf428b
ClayBuilds <117887075+ClayBuilds@users.noreply.github.com>
2025-04-19 17:01:40 -0500

    add line for Linux serial connection, examples

--- Internal SD Card Data Transfer/PC/SDWRITETOARDUINO/SDWRITETOARDUINO.py
+++ Internal SD Card Data Transfer/PC/SDWRITETOARDUINO/SDWRITETOARDUINO.py
@@ -1,15 +1,19 @@
 #SDWRITETOARDUINO
-#Version 1, 6/8/24
-#this program takes some existing .txt files from the PC and sends them to an arduino over serial. 
+#Version 4/19/25
+#This code written for Gumption Trap II Project
+#this program takes some existing .txt files from the PC and sends them to the boat's onboard SD card through the main microcontroller over serial via its main USB cable connetion. 
 #note: if files of the same name already exist on the SD card, this program will append to the end of them rather than overwrite. arduino program will be configured to delete old contents before writing what it receives from this script
+
+#Local backup version 1
 #tested and everything works as intended
 
 import serial
 import os
 import time
 
-# Replace with your actual serial port and baud rate
-serial_port = 'COM3'  # Update this to your Arduino's serial port. port 3 on my laptop, 4 on my desktop
+serial_port = '/dev/ttyACM0'   # Arduino Serial port (Linux),   actual # may vary
+# serial_port = 'COM3'         # Arduino serial port (Windows), actual # may vary
+
 baud_rate = 9600
 folder_path = './filestosend'  # Update this to the path of your folder containing .txt files
 
